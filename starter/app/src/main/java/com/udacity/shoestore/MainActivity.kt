bc9be55cd9800dddda41e2706e458a625a6a0759
esegebart <36378900+esegebart@users.noreply.github.com>
2020-12-22 15:24:01 -0600

    [+] Navigation.xml and data binding

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.udacity.shoestore.R.layout.activity_main
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, activity_main)
         //setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
